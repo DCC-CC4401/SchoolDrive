18108552c400dda9df6de87d75e00ee9d4f6e79a
Bryan Ortiz <bryan_alexis.7@hotmail.com>
2020-05-04 21:38:53 -0400

    View del index

--- drive/views.py
+++ drive/views.py
@@ -14,32 +14,54 @@ from .models import Task, Category
 #   The user can enter information and then send a POST request to add a task to the list
 #   This method has the main functionality of the project
 def index(request): #the index view
-    if request.user.is_authenticated:
-        todos = Task.objects.filter(owner=request.user) #quering all todos with the object manager
-    else:
-        todos= Task.objects.filter(owner=None)
-    categories = Category.objects.all() #getting all categories with object manager
-    if request.method=="GET":
-        return render(request, "drive/index.html", {"todos": todos, "categories": categories})
+    # if request.user.is_authenticated:
+    #     todos = Task.objects.filter(owner=request.user) #quering all todos with the object manager
+    # else:
+    #     todos= Task.objects.filter(owner=None)
+    # categories = Category.objects.all() #getting all categories with object manager
+    # if request.method=="GET":
+    #     return render(request, "drive/index.html", {"todos": todos, "categories": categories})
+    if request.method == 'GET':
+        return render(request, "drive/index.html")
     if request.method == "POST": #checking if the request method is a POST
-        if "taskAdd" in request.POST: #checking if there is a request to add a todo
-            title = request.POST["description"] #title
-            date = str(request.POST["date"]) #date
-            category = request.POST["category_select"] #category
-            content = title + " -- " + date + " " + category #content
-            if request.user.is_authenticated:
-                Todo = Task(title=title, content=content, due_date=date, category=Category.objects.get(name=category),owner=request.user)
+        
+        if 'login' in request.POST: #Log In
+            username = request.POST['username']
+            contraseña = request.POST['contraseña']
+            usuario = authenticate(request,username=username,password=contraseña)
+            if usuario is not None:
+                login(request, usuario)
+                messages.success(request, 'Te damos la bienvenida ' + usuario.apodo + '!')
+                return HttpResponseRedirect('/')
             else:
-                Todo = Task(title=title, content=content, due_date=date, category=Category.objects.get(name=category))
-            Todo.save() #saving the todo
-            return redirect("/") #reloading the page
-        if "taskDelete" in request.POST: #checking if there is a request to delete a todo
-            checkedlist = request.POST["checkedbox"] #checked todos to be deleted
-            for todo_id in checkedlist:
-                print(todo_id,"Todo id")
-                todo = Task.objects.get(id=int(todo_id)) #getting todo id
-                todo.delete() #deleting todo
-            return render(request, "drive/index.html", {"todos": todos, "categories": categories})
+                return HttpResponseRedirect('/register')
+        else: #Register
+            nombre = request.POST['nombre']
+            contraseña = request.POST['contraseña']
+            apodo = request.POST['apodo']
+            mail = request.POST['mail']
+            user = User.objects.create_user(username=nombre, password=contraseña,email=mail,apodo=apodo)
+            messages.success(request, 'Se creó el usuario para ' + user.apodo + '!')
+            return HttpResponseRedirect('/')
+
+        # if "taskAdd" in request.POST: #checking if there is a request to add a todo
+        #     title = request.POST["description"] #title
+        #     date = str(request.POST["date"]) #date
+        #     category = request.POST["category_select"] #category
+        #     content = title + " -- " + date + " " + category #content
+        #     if request.user.is_authenticated:
+        #         Todo = Task(title=title, content=content, due_date=date, category=Category.objects.get(name=category),owner=request.user)
+        #     else:
+        #         Todo = Task(title=title, content=content, due_date=date, category=Category.objects.get(name=category))
+        #     Todo.save() #saving the todo
+        #     return redirect("/") #reloading the page
+        # if "taskDelete" in request.POST: #checking if there is a request to delete a todo
+        #     checkedlist = request.POST["checkedbox"] #checked todos to be deleted
+        #     for todo_id in checkedlist:
+        #         print(todo_id,"Todo id")
+        #         todo = Task.objects.get(id=int(todo_id)) #getting todo id
+        #         todo.delete() #deleting todo
+        #     return render(request, "drive/index.html", {"todos": todos, "categories": categories})
 
 #Called with /register, it is available if the user is not authenticated, and renders
 #   the view where the user can enter the necessary to have an account.
@@ -64,16 +86,17 @@ def login_user(request):
     if request.method == 'GET':
         return render(request, "drive/login.html")
     if request.method == 'POST':
-        username = request.POST['username']
-        contraseña = request.POST['contraseña']
-        usuario = authenticate(request,username=username,password=contraseña)
-        if usuario is not None:
-            login(request, usuario)
-            messages.success(request, 'Te damos la bienvenida ' + usuario.apodo + '!')
-            return HttpResponseRedirect('/')
-        else:
-            return HttpResponseRedirect('/register')
-
+        
+            username = request.POST['username']
+            contraseña = request.POST['contraseña']
+            usuario = authenticate(request,username=username,password=contraseña)
+            if usuario is not None:
+                login(request, usuario)
+                messages.success(request, 'Te damos la bienvenida ' + usuario.apodo + '!')
+                return HttpResponseRedirect('/')
+            else:
+                return HttpResponseRedirect('/register')
+        
 #Called with /logout, only available if the user is authenticated
 #   Logouts the authenticated user and shows the screen as if is not registered
 def logout_user(request):
