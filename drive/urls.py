ff8f0415c52e40b5931ffc66fb74a25852093ec1
LukasGL <lukas.gribbell@ug.uchile.cl>
2020-06-15 22:01:50 -0400

    Added new view/url "files"

--- drive/urls.py
+++ drive/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home_page'),
     path('logout',views.logout_user, name='logout'),
-    path('profile', views.view_profile, name='profile')
+    path('profile', views.view_profile, name='profile'),
+    path('files', views.view_files, name='files')
 ]
