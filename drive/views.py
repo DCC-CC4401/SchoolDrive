fd0d6d21e36c3f38716670b4494a38bf93697e3e
Bryan <bryortizp@gmail.com>
2020-06-22 14:49:14 -0400

    Borrar Archivos
    Se pueden borrar :)

--- drive/views.py
+++ drive/views.py
@@ -4,22 +4,20 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.shortcuts import render,redirect
 from .models import User, Archivo, Carpeta
-#from .models import Task, Category
 from datetime import *
 import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-#El Index lo documente por ahora para el todolist, pero de ahi lo cambiamos, no lo quiero borrar porque a futuro
-#   puede ser util tenerlo.
+AVATAR_ROOT = os.path.join(BASE_DIR, 'media/avatars')
+FILES_ROOT  = os.path.join(BASE_DIR, 'media/archivos')
+# El Index lo documente por ahora para el todolist, pero de ahi lo cambiamos, no lo quiero borrar porque a futuro
+# puede ser util tenerlo.
 
-#Shows the index of the project. It shows different information depending if the
-#   user is authenticated or not.
-#   The user can enter information and then send a POST request to add a task to the list
-#   This method has the main functionality of the project
-def index(request): #the index view
-    #folders = Carpeta.objects.all()
-    #todos = Task.objects.filter(owner=request.user)
+# Shows the index of the project. It shows different information depending if the
+# user is authenticated or not.
+# The user can enter information and then send a POST request to add a task to the list
+def index(request): # the index view
+    
     if request.method == 'GET':
         return render(request, "drive/index.html")
     if request.method == "POST": #checking if the request method is a POST
@@ -27,7 +25,7 @@ def index(request): #the index view
         if 'login' in request.POST: #Log In
             mail = request.POST['mail']
             contraseña = request.POST['contraseña']
-            print('mail:' +mail+'pass: '+contraseña) ## No loguea :c
+            #print('mail:' +mail+'pass: '+contraseña)
             usuario = authenticate(request,username=mail,password=contraseña)
             if usuario is not None:
                 login(request, usuario)
@@ -36,6 +34,7 @@ def index(request): #the index view
             else:
                 messages.error(request, 'No hubo match para los datos ingresados.')
                 return HttpResponseRedirect('/')
+
         else: #Register
             nombre = request.POST['Nombre']
             apellido =  request.POST['Apellido']
@@ -53,14 +52,14 @@ def index(request): #the index view
                 return HttpResponseRedirect('/')
             return HttpResponseRedirect('/')
         
-#Called with /logout, only available if the user is authenticated
-#   Logouts the authenticated user and shows the screen as if is not registered
+# Called with /logout, only available if the user is authenticated
+# Logouts the authenticated user and shows the screen as if is not registered
 def logout_user(request):
     logout(request)
     return HttpResponseRedirect('/')
 
-#Called with /profile, only available por aunthenticated users and shows
-#   the user information displayed in the screen
+# Called with /profile, only available por aunthenticated users and shows
+# the user information displayed in the screen
 @login_required
 def view_profile(request):
     
@@ -70,16 +69,16 @@ def view_profile(request):
     if request.method == "POST": #checking if the request method is a POST
         usuario = request.user
 
-        if 'profilepic' in request.POST: #Log In
+        if 'profilepic' in request.POST: #Edit Picture
             
             if request.FILES['adjunto']:
                 old = usuario.avatar
                 usuario.avatar  = request.FILES['adjunto']
                 usuario.avatar.name = usuario.first_name + "##" + usuario.last_name + "##" + str(datetime.now()) +".png"
                 if old != None:
-                    os.remove(os.path.join(MEDIA_ROOT, old.name))
+                    os.remove(os.path.join(AVATAR_ROOT, old.name))
 
-        else:
+        else: # Edit Info
             usuario.first_name       = request.POST['Nombre']
             usuario.last_name        = request.POST['Apellido']
             usuario.apodo            = request.POST['Apodo']
@@ -92,12 +91,11 @@ def view_profile(request):
         messages.success(request, 'Se modificaron tus datos!')
         return HttpResponseRedirect('/profile')
 
-
-    #Dejo aqui abierto por si queremos hacer un post que cambie los atributos
-    pass
-
+# Called with /files, only available por aunthenticated users and shows
+# the user files and folders
 @login_required
 def view_files(request):
+
     files = Archivo.objects.filter(usuario=request.user)
     folders = Carpeta.objects.filter(usuario=request.user)
     formatos_img = ['jpg','png','jepg','gif']
@@ -110,11 +108,10 @@ def view_files(request):
     "formatos_vid": formatos_vid, "formatos_msc":formatos_msc}
 
     if request.method == 'GET':
-        return render(request, "drive/datafiles.html",diccionario)
+        return render(request, "drive/datafiles.html", diccionario)
 
     if request.method == "POST": #checking if the request method is a POST
-
-        if request.FILES['archivo']:
+        if 'Upload_file' in request.POST and  request.FILES['archivo']:
             archivo = request.FILES['archivo'] # archivo
             carpeta_name = request.POST['folder']  # seleccionar carpeta
             nombre  = request.POST['nombre']
@@ -130,6 +127,25 @@ def view_files(request):
             # Modifica valores
             return HttpResponseRedirect('/files')
             messages.success(request, 'Archivo subido!')
+        
+        elif 'deleteFiles' in request.POST:
+            toDelete = []
+            for element in request.POST:
+                toDelete.append(element)
+            toDelete = toDelete[2:] #Primeros 2 elementos no importan
+            filesToDelete = []
+            for fileRoute in toDelete:
+                folder = fileRoute.split("/")[0]
+                rest   = fileRoute.split("/")[1]
+                f_name = rest.split(".")[0]
+                formato= rest.split(".")[1]
+                archivo = Archivo.objects.get(nombre=f_name,formato=formato,usuario=request.user,carpeta=folder)
+                archivo.delete()
+                os.remove(os.path.join(FILES_ROOT, f_name+"."+formato))
+                print(archivo.nombre+" borrado satisfactoriamente :)")
+            messages.success(request, 'Archivos borrados!')
+            return HttpResponseRedirect('/files')
+
         # editar avatar
     #Dejo aqui abierto por si queremos hacer un post que cambie los atributos
     pass
