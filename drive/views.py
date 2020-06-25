ecbc5365c7193b891b8d1a2cd35316336933bdb7
Diego Hurtado <diegohurtado@ug.uchile.cl>
2020-06-25 10:51:17 -0400

    Me falto un import
    ups

--- drive/views.py
+++ drive/views.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
